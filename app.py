97fe6510bff97ebf996cae3b2f47b285a422bca1
cryptojenn <cryptojenn.altcoinqueen@yahoo.com>
2023-10-13 00:03:04 -0500

    Resave

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-# import models
+from models import (Base, session, Book, engine)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
 # main menu - add, search, analysis, exit, view
 # add books to the database
 # edit
--- models.py
+++ models.py
@@ -1,15 +1,26 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import (create_engine, 
+                        Column, Integer, String, Date)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # create db
-engine = create_engine('sqlite:///users.db', echo=True)
-Base = declarative_base
+engine = create_engine('sqlite:///books.db', echo=False)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-Session = engine
-session = session()
+class Book(Base):
+    __tablename__ = 'books'
 
-def_tablename == 'books'
+    id = Column(Integer, primary_key =True)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published_date = Column('Published', Date)
+    price = Column('Price', Integer)
+
+def __repr__(self):
+    return f'Title: {self.title}, Author: {self.author}, Published: {self.published_date}, Price: {self.price}'
+
 
 # books.db
 # create a model
